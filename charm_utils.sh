1cee199c0dbb77c5e931b35f295a57c8f7f77979
utkarsh bhatt <utkarsh.bhatt@canonical.com>
2025-02-13 15:02:48 +0530

    Refactor ceph_metrics as a library

---
+++ charm_utils.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+function build() {
+  local charm="${1?missing}"
+
+  # remove old and copy fresh dependencies into lib dir.
+  rm -rf $charm/lib/charms.ceph*
+  cp -r ./charms.ceph* $charm/lib/
+
+  cd $charm
+  tox -e build
+}
+
+function clean() {
+  local charm="${1?missing}"
+
+  # remove old and copy fresh dependencies into lib dir.
+  rm -rf $charm/lib/charms.ceph*
+  cd $charm
+  charmcraft clean
+}
+
+run="${1}"
+shift
+
+$run "$@"
