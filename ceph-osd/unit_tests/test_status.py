c42508be583a9b372c6625f58f7c48c13b79f930
Vinit Chacharia <vinit.chacharia@canonical.com>
2025-02-18 15:06:10 +0530

    Fix update-status hook to not run smartctl

--- ceph-osd/unit_tests/test_status.py
+++ ceph-osd/unit_tests/test_status.py
@@ -15,7 +15,30 @@
 from unittest import mock
 import test_utils
 import os
-
+from charmhelpers.core import hookenv
+from charmhelpers.core.hookenv import (
+    log,
+    DEBUG,
+    ERROR,
+    INFO,
+    WARNING,
+    config,
+    relation_ids,
+    related_units,
+    relation_get,
+    relation_set,
+    relations_of_type,
+    Hooks,
+    local_unit,
+    UnregisteredHookError,
+    service_name,
+    status_get,
+    status_set,
+    storage_get,
+    storage_list,
+    application_version_set,
+    hook_name,
+)
 from unittest.mock import MagicMock, patch
 
 with patch('charmhelpers.contrib.hardening.harden.harden') as mock_dec:
@@ -136,15 +159,19 @@ class ServiceStatusTestCase(test_utils.CharmTestCase):
             'blocked', 'Invalid configuration: fake-config is invalid')
 
     @patch.object(hooks, 'get_bdev_enable_discard')
-    def test_assess_status_update_status(self, mock_update):
-        self.relation_ids.return_value = []
+    def test_assess_status_update_status(self, mock_get_bdev_enable_discard):
+        self.relation_ids.return_value = ['mon:1']
         os.environ['JUJU_HOOK_NAME'] = 'update-status'
         hooks.assess_status()
-        mock_update.assert_not_called()
+        mock_get_bdev_enable_discard.assert_not_called()
 
     @patch.object(hooks, 'get_bdev_enable_discard')
-    def test_assess_status_config_changed(self, mock_config_changed):
-        self.relation_ids.return_value = []
+    @patch.object(hooks.ch_context, 'CephBlueStoreCompressionContext',
+                  lambda: MagicMock())
+    def test_assess_status_config_changed(self, mock_get_bdev_enable_discard):
         os.environ['JUJU_HOOK_NAME'] = 'config-changed'
+        self.relation_ids.return_value = ['mon:1']
+        self.get_conf.return_value = 'monitor-bootstrap-key'
+        mock_get_bdev_enable_discard.side_effect = ValueError('Mocked error')
         hooks.assess_status()
-        mock_config_changed.assert_called()
+        mock_get_bdev_enable_discard.assert_called()
