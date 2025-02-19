eda06b1d4a11508a3b73975eeaa9373ecaf76b67
Vinit Chacharia <vinit.chacharia@canonical.com>
2025-02-19 23:12:34 +0530

    Fix update-status hook to not run smartctl

--- ceph-osd/unit_tests/test_status.py
+++ ceph-osd/unit_tests/test_status.py
@@ -135,23 +135,23 @@ class ServiceStatusTestCase(test_utils.CharmTestCase):
         self.status_set.assert_called_with(
             'blocked', 'Invalid configuration: fake-config is invalid')
 
-    @patch.object(hooks, 'get_bdev_enable_discard')
+    @patch.object(hooks, 'should_enable_discard')
     @patch.object(hooks.ch_context, 'CephBlueStoreCompressionContext',
                   lambda: MagicMock())
-    def test_assess_status_update_status(self, mock_get_bdev_enable_discard):
+    def test_assess_status_update_status(self, mock_should_enable_discard):
         self.relation_ids.return_value = ['mon:1']
         os.environ['JUJU_HOOK_NAME'] = 'update-status'
         self.get_conf.return_value = 'monitor-bootstrap-key'
         hooks.assess_status()
-        mock_get_bdev_enable_discard.assert_not_called()
+        mock_should_enable_discard.assert_not_called()
 
-    @patch.object(hooks, 'get_bdev_enable_discard')
+    @patch.object(hooks, 'should_enable_discard')
     @patch.object(hooks.ch_context, 'CephBlueStoreCompressionContext',
                   lambda: MagicMock())
-    def test_assess_status_config_changed(self, mock_get_bdev_enable_discard):
+    def test_assess_status_config_changed(self, mock_should_enable_discard):
         os.environ['JUJU_HOOK_NAME'] = 'config-changed'
         self.relation_ids.return_value = ['mon:1']
         self.get_conf.return_value = 'monitor-bootstrap-key'
-        mock_get_bdev_enable_discard.side_effect = ValueError('Mocked error')
+        mock_should_enable_discard.side_effect = ValueError('Mocked error')
         hooks.assess_status()
-        mock_get_bdev_enable_discard.assert_called()
+        mock_should_enable_discard.assert_called()
