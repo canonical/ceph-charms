9cfdd56103cb0473dcc99b868dd8e180771b1043
utkarsh bhatt <utkarsh.bhatt@canonical.com>
2025-02-24 15:24:05 +0530

    Move individual charms to building from local libraries

--- charm_utils.sh
+++ charm_utils.sh
@@ -1,22 +1,14 @@
 #!/bin/bash
 
-function build() {
-  local charm="${1?missing}"
-
-  # remove old and copy fresh dependencies into lib dir.
-  rm -rf $charm/lib/charms.ceph*
-  cp -r ./charms.ceph* $charm/lib/
-
-  cd $charm
-  tox -e build
-}
-
 function copy_libs() {
   local charm="${1?missing}"
 
-  # remove old and copy fresh dependencies into lib dir.
-  rm -rf $charm/lib/charms.ceph*
+   # remove old copy of local libraries may fail if lib folder does not exist. 
+  rm -rf $charm/lib/charms.ceph* || true
+  # create a lib folder in case it does not exist
+  mkdir -p $charm/lib
   cp -r ./charms.ceph* $charm/lib/
+
 }
 
 function clean() {
